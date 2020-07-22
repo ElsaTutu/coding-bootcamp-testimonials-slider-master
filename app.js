e86b8d5fece6844aa77e1543086a95e299bca89e
ElsaTutu <elsa.tuilliere@gmail.com>
2020-07-22 09:10:43 +0100

    arrangement on the desktop view

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ function showSlides(n) {
       slides[i].style.display = "none";
   }
   
-  slides[slideIndex-1].style.display = "block";
+  
+slides[slideIndex-1].style.display = "block";
+
   
 } 
